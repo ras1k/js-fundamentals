40864b4ebff154b0d2a38c5eca47aee7855852b2
ras1k <rasikabdullah@gmail.com>
2023-02-28 20:13:02 +0600

    array loop

---
+++ arr-loop.js
@@ -0,0 +1,9 @@
+// var array = [12, 23, 34, 45, 56, 67, 78, 89];
+// for(var i=0; i<10; i++){
+//     console.log(array[i]);
+// }
+
+var items = ['bottles', 'sunglass', 'burger'];
+for (var i = 0; i<items.length; i++){
+    console.log(items[i]);
+}
---
+++ odd-number.js
@@ -0,0 +1,9 @@
+var number = 1;
+// while (number <= 10){
+//     console.log(number);
+//     number = number+2;
+// }
+for(number; number<=10; number+=3){
+    console.log(number);
+}
+
