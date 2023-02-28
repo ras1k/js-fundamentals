3959a79451be98c57baa3dd6b6d768237db3b64c
ras1k <rasikabdullah@gmail.com>
2023-03-01 01:42:09 +0600

    new problems added

---
+++ comparison.js
@@ -0,0 +1,12 @@
+var a = 913;
+var b = 79;
+var c = 145;
+if (a > b && a > c){
+    console.log('a is the biggest');
+}
+else if ( b > a  && b>c){
+    console.log('b is the greatest');
+}
+else {
+    console.log('c is the biggest');
+}
---
+++ triangle.js
@@ -0,0 +1,9 @@
+var a = 9;
+var b = 8;
+var c = 8;
+if (a == b || a == c || b == c){
+    console.log('it is Isosceles');
+}
+else {
+    console.log('it is not ')
+}
