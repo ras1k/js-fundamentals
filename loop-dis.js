56391f3d0a1aa2228cc5ab158d91948f08bae2b2
ras1k <rasikabdullah@gmail.com>
2023-03-01 19:28:33 +0600

    another problems added

---
+++ loop-dis.js
@@ -0,0 +1,28 @@
+// var a = 0;
+// while(a<=39){
+//     console.log('ajke amar mon valo nei');
+//     a++;
+// }
+
+// for(var i =58; i<=98; i++){
+//     console.log(i, 'ajke amar mon valo nei')
+// }
+
+// for(var i =412; i<=498; i+=2){
+//     console.log('Even Number', i);
+// }
+
+// for(var i =581; i<=623; i+=2){
+//     console.log('Odd Number', i);
+// }
+
+// var l = ['html', 'css', 'bootstrap', 'tailwind', 'git', 'javascript'];
+// for (var i = 0; i < l.length; i++){
+//     console.log(l[i]);
+// }
+var a =0;
+var phones =['nokia', 'samsung', 'helio', 'lumia', 'xiaomi', 'redmi', 'poco'];
+while (a < phones.length){
+    console.log(phones[a]);
+    a++;
+}
--- triangle.js
+++ triangle.js
@@ -1,7 +1,7 @@
 var a = 9;
 var b = 8;
 var c = 8;
-if (a == b || a == c || b == c){
+if (a == b || a == c || b == c) {
     console.log('it is Isosceles');
 }
 else {
